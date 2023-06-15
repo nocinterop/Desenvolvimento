64f5ef943015d08f305a3bf55b18f5aae1f274ca
Noc Interop <125463923+nocinterop@users.noreply.github.com>
2023-06-15 16:53:43 -0300

    Add files via upload

---
+++ Zabbix/Template Microsoft SQL Server/README.md
@@ -0,0 +1,39 @@
+**Equipe de Desenvolvimento**
+
+Este repositório contém os scripts desenvolvidos pela equipe de desenvolvimento do Projeto XYZ. Aqui estão alguns detalhes sobre nossa equipe e como utilizar os scripts disponíveis.
+
+**Integrantes:**
+- Leonardo Soares dos santos
+- Guilherme Mendes de Oliveira
+
+**Sobre o Time:**
+O time de desenvolvimento visa criar monitorias para resolver um problema específico de um cliente ou templates padrão para Zabbix e Priax. Neste repositório, você encontrará os scripts e templates que nossa equipe desenvolveu.
+
+**Organização do Repositório**
+
+- **/Zabbix:** Este diretório contém os scripts relacionados aos templates de Zabbix.
+
+- **/Priax:** Aqui estão os scripts relacionados ao desenvolvimento de Priax.
+
+
+
+**Como Utilizar os Scripts:**
+Para utilizar os scripts deste repositório, siga as etapas abaixo:
+
+Faça um clone deste repositório em sua máquina local.
+Acesse o diretório específico do script que deseja utilizar (por exemplo, /zabbix para os scripts do zabbix).
+Siga as instruções de documentação disponíveis em cada diretório para configurar e executar os scripts corretamente.
+
+
+**Contribuindo:**
+Agradecemos seu interesse em contribuir para o Desenvolvimento! Se você deseja colaborar, por favor, siga as orientações abaixo:
+
+Crie um fork deste repositório em sua conta do GitHub.
+Realize as modificações ou adições desejadas em seu fork.
+Envie um pull request com suas alterações para revisão.
+
+**Contato:**
+Se você tiver alguma dúvida ou sugestão relacionada ao Projeto XYZ ou aos scripts deste repositório, sinta-se à vontade para entrar em contato com nossa equipe:
+
+**Email**: leonardo.santos@interop.com.br
+Agradecemos sua visita e interesse em nosso projeto!
---
+++ Zabbix/VmWare Esxi 6/Scripts/Discovery_DataStore_Network.ps1
@@ -0,0 +1,226 @@
+<# 
+OpÃ§Ãµes indicador = power, temp
+UserName= 'priax-monitor'
+Password='f6v%d&5>89@7'
+
+Example: .\check_datastore_nic.ps1'
+
+#>
+
+# main function
+
+Param (
+    [Parameter(Mandatory=$true)][string]$HostEsxi,
+	[Parameter(Mandatory=$true)][string]$Option,
+	[Parameter(Mandatory=$true)][string]$Mode,
+	[Parameter(Mandatory=$true)][string]$Target
+    #[Parameter(Mandatory=$true)][string]$Indicator
+    )
+	
+function targetDSExists
+{
+	param($data)
+	foreach ($d in $data)
+	{
+		if ($Target -eq $d.diskname) {
+			return $true
+		} else {
+			return $false
+		}
+	}
+}
+
+
+function targetNetExists
+{
+	param($data)
+	foreach ($d in $data)
+	{
+		if ($Target -eq $d.nameinterface) {
+			return $true
+		} else {
+			return $false
+		}
+	}
+}
+
+
+
+function getReceicedBytes
+{
+	foreach ($d in $data)
+    {
+		foreach ($nic in $d.all_network)
+		{
+			if (targetNetExists -data $nic) 
+			{
+				return $nic.received
+			} 
+			else
+			{
+				continue
+			}
+		}
+    }
+    throw "Not Found DS: $Target in Host: $HostEsxi"
+
+}
+
+function getSentBytes
+{
+	foreach ($d in $data)
+    {
+		foreach ($nic in $d.all_network)
+		{
+			if (targetNetExists -data $nic) 
+			{
+				return $nic.transmitted
+			} 
+			else
+			{
+				continue
+			}
+		}
+    }
+    throw "Not Found DS: $Target in Host: $HostEsxi"
+
+}
+
+
+
+
+function getTotalSpace
+{
+	foreach ($d in $data)
+    {
+		foreach ($disk in $d.all_disk)
+		{
+			if (targetDSExists -data $disk) 
+			{
+				return $disk.disktotal
+			} 
+			else
+			{
+				continue
+			}
+		}
+    }
+    throw "Not Found DS: $Target in Host: $HostEsxi"
+}
+
+
+function getFreeSpace
+{
+    # return percentage of free space
+
+    foreach ($d in $data)
+    {
+		foreach ($disk in $d.all_disk)
+		{
+			if (targetDSExists -data $disk)
+			{
+				return $disk.freediskperc
+			}
+			else
+			{
+				continue
+			}
+		}
+	}
+	throw "Not Found DS: $Target in Host: $HostEsxi"
+}
+
+
+function getDataStore 
+{
+    switch -wildcard ($Option)
+    {
+        "totalSpace"
+        {
+            return getTotalSpace
+        }
+        "FreeSpace"
+        {
+            return getFreeSpace
+        }
+        default
+        {
+            throw "Invalid option selected"
+        }        
+    }
+    Write-Host Target = "$Target"
+}
+
+function getNetwork
+{
+    switch -wildcard ($Option)
+    {
+        "bytesreceived"
+        {
+            return getReceicedBytes
+        }
+        "bytessent"
+        {
+            return getSentBytes
+        }
+        default
+        {
+            throw "Invalid option selected"
+        }        
+    }
+    Write-Host Target = "$Target"
+
+
+}
+
+function ReadVmWareData
+{
+	for($i=0;$i -lt 3;$i++)
+	{
+		try
+        {
+            $data = Get-Content -Path $path -ErrorAction Stop -Raw | ConvertFrom-Json
+            return $data
+        } 
+		catch [System.IO.IOException] 
+        {
+            $data = $false
+            start-sleep 1
+		}
+	}
+	return $data
+
+}
+
+
+
+function Run
+{
+    switch -wildcard ($Mode)
+	{
+	    "ds"
+        {
+            return getDataStore
+        }
+        "net"
+        {
+            return getNetwork
+        } 
+        default
+        {
+            throw "Invalid mode selected!"
+        }
+    }
+}
+
+
+function main 
+{
+    $path = "C:\temp\disk-nic-data-" + $HostEsxi + ".json"
+	$data = ReadVmWareData -path $path
+	
+    $resp = Run
+    write-output $resp
+}
+
+main
